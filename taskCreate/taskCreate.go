f41f927a0b584156e7d0925bc02dcef9ed6a20d9
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2024-03-18 17:45:10 -0300

    Merge das funções no Round Robin

--- taskCreate/taskCreate.go
+++ taskCreate/taskCreate.go
@@ -10,34 +10,38 @@ type taskStruct struct {
 	Nome     string
 	Id       int
 	Priority int
-	Quantum  float64
-	Status   string
+	Quantum  int
+	//Status   string
 }
 
 func Taskcreate(numero int, Nquantum int, Npriority int) taskStruct {
 	stringNumero := strconv.Itoa(numero)
 
 	task := taskStruct{}
-	task.Nome = "Process" + stringNumero
+	task.Nome = "Process " + stringNumero
 
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	task.Id = r.Intn(201) + 1
 
 	quantuns := [100]int{
-		20, 78, 47, 86, 93, 88, 85, 90, 82, 98, 68, 96, 52, 84, 74, 46, 62, 49, 66, 72,
-		92, 73, 97, 91, 89, 99, 65, 63, 21, 27, 48, 61, 83, 51, 60, 75, 29, 26, 28, 67,
-		69, 80, 22, 57, 94, 77, 55, 70, 87, 79, 23, 64, 95, 81, 24, 76, 25, 71, 30, 54,
-		58, 56, 53, 31, 100, 59, 50, 45, 32, 33, 34, 36, 37, 35, 38, 39, 40, 41, 42, 43,
-		44, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100,
+		127, 51, 73, 222, 147, 126, 135, 139, 149, 70, 77,
+		243, 29, 156, 134, 120, 99, 112, 261, 54, 48, 245,
+		56, 208, 243, 35, 91, 230, 39, 35, 265, 151, 26,
+		95, 94, 221, 31, 181, 287, 201, 240, 166, 257, 243,
+		298, 38, 250, 49, 115, 213, 215, 37, 194, 126, 21, 99,
+		80, 258, 39, 76, 57, 227, 82, 150, 211, 60, 204, 293,
+		248, 240, 294, 74, 224, 276, 226, 59, 258, 261, 34, 234,
+		248, 93, 251, 198, 172, 111, 80, 103, 52, 282, 48, 284,
+		192, 100, 184, 137, 194, 54, 272, 271,
 	}
 
 	priorities := [11]int{
-		7, 4, 11, 8, 3, 10, 5, 6, 2, 9,
+		7, 1, 10, 8, 3, 10, 5, 6, 2, 9, 4,
 	}
 
-	task.Quantum = float64(quantuns[Nquantum])
+	task.Quantum = quantuns[Nquantum]
 	task.Priority = priorities[Npriority]
-	task.Status = "disponivel"
+	//task.Status = "disponivel"
 
 	return task
 
@@ -45,13 +49,13 @@ func Taskcreate(numero int, Nquantum int, Npriority int) taskStruct {
 
 func TaskVetorCreator(QuantidadeDeTask int) []taskStruct {
 
-	taskVetor := make([]taskStruct, 10)
+	taskVetor := make([]taskStruct, QuantidadeDeTask)
 
 	for i := 0; i < QuantidadeDeTask; i++ {
 
 		newTask := Taskcreate(i+1, i, i)
 
-		taskVetor = append(taskVetor, newTask)
+		taskVetor[i] = newTask
 
 	}
 
